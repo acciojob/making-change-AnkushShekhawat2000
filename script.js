377a4702fda6a41326ab26c9d1418cc09e36d8ca
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 10:05:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const makeChange = (c) => {
-  // your name here
+  // Initialize variables to store the number of quarters, dimes, nickels, and pennies
+  let q = 0, d = 0, n = 0, p = 0;
+
+  // Calculate the number of quarters
+  q = Math.floor(c / 25);
+  c %= 25;
+
+  // Calculate the number of dimes
+  d = Math.floor(c / 10);
+  c %= 10;
+
+  // Calculate the number of nickels
+  n = Math.floor(c / 5);
+  c %= 5;
+
+  // The remaining cents represent the number of pennies
+  p = c;
+
+  // Return an object with the calculated values
+  return { q, d, n, p };
 };
 
-// Do not the change the code below
+// Example
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
